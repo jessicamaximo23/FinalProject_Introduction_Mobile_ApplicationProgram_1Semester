cf9591d0c0ed3d929c6f9b05f79ef5fdc41dbe81
Jessica Maximo <jessicamaximo23.com>
2023-09-09 21:16:47 -0400

    update

--- src/Project/Computer.java
+++ src/Project/Computer.java
@@ -1,5 +1,7 @@
 package Project;
 
+import java.util.Scanner;
+
 public class Computer {
 	// -------------------------------------------------------------------------------------------------------------
 	// Project Final
@@ -101,5 +103,216 @@ public class Computer {
 		} else {
 			return false;
 		}
+	}
+
+	// --------------------------------------------------
+	// Here are the part B of the program java
+	// We need tracking computers
+	// --------------------------------------------------
+
+	public static void main(String[] args) {
+		// Display a Welcome message
+		System.out.println("Welcome to this program, here we can tracking the computer that you want ");
+		System.out.println("-------------------------------------------------------------------------");
+		Scanner kb = new Scanner(System.in);
+
+		int maxComputers = 0;
+		// initialize the variable main menu
+		int choiceMenu;
+		// initialize the variable for add new computers
+		int addNewComputers = 0;
+		// initialize update computers
+		int updateComputersNumber = 0;
+
+		while (maxComputers == 0) {
+			// owner put the number maximum of computers (Size or maxComputers)
+			System.out.println("Whats the maximum numbers of computers that your store can contain? ");
+			maxComputers = kb.nextInt();
+
+			// test only with positives numbers
+			if (maxComputers < 1) {
+				System.out.println("Invalid, try again. The number should be positve");
+			}
+		}
+		// Create a empty array (inventory)
+		// Tracking the object computer - previously created
+		Computer inventory[] = new Computer[maxComputers];
+
+		// Put do and while for continue show the main menu
+		do {
+			// Display a main menu
+			System.out.println("You are on the Main menu:  ");
+			System.out.println("---------------------------------------------------------------------------------");
+			System.out.println("Option 1: Enter new computers (Password Required)");
+			System.out.println("Option 2: Change information of a computer (Password required)");
+			System.out.println("Option 3: Display all computers by a specific brand");
+			System.out.println("Option 4: Display all computers under a certain a price");
+			System.out.println("Option 5: Quit");
+			System.out.println("---------------------------------------------------------------------------------");
+			System.out.println("Please enter your choice > ");
+			// initialized a variably menu
+			// user choose a option between 1 and 5
+			choiceMenu = kb.nextInt();
+
+			// Each main menu
+			switch (choiceMenu) {
+			case 1:
+
+				System.out.println("You choose 1.Put your password: ");
+				// if the correct password is check put how many computers do want to add
+				if (checkPassword(kb)) {
+					System.out.println("How many computers do want to add?");
+					addNewComputers = kb.nextInt();
+
+					// check if has enough space in the computer store
+					// if the new computers are more than capacity computers
+					if (addNewComputers > maxComputers) {
+						System.out.println("Please choose other number. This number exceeds tha maximum capacity");
+					}
+				}
+
+				// add new computers to the inventory
+				// put the quantity of the computers
+				for (int i = 0; i < addNewComputers; i++) {
+
+					// i+1 because need show "computer 1" instead "computer 0"
+					System.out.println("Computer #: " + (i + 1));
+					System.out.println("Computer brand is: ");
+					String brand = kb.next();
+
+					System.out.println("Computer price is: ");
+					double price = kb.nextDouble();
+
+					System.out.println("Computer model is: ");
+					String model = kb.next();
+
+					System.out.println("Computer serial number is: ");
+					long serialNumber = kb.nextLong();
+
+					System.out.println();
+					computerCounter++;
+
+					// nao sei como acrescentar esse pc no programa geral
+					// nao mostrar toda vez o main menu aqui
+					Computer inventoryaddNew[] = new Computer[addNewComputers];
+
+				}
+				break;
+
+			case 2:
+
+				do {
+					System.out.println("You choose 2. Put your password");
+					// if the correct password is check put how many computers do want to update
+
+					if (checkPassword(kb)) {
+						System.out.println("Which computer number do you want update?");
+
+						// variable for update the computers
+						updateComputersNumber = kb.nextInt();
+
+						// for to add a computer we need to find the index, because computer already
+						// exist
+						int index = updateComputersNumber - 1;
+
+						// The computer number is the index in the array inventory.
+
+						if (index > 0 && index >= computerCounter) {
+
+							Computer c = inventory[index];
+
+							// ARRUMAR AQUI TA DANDO NULL NO GET
+							// como salvar as in formacoes que digitei?
+							// display the current information of that computer in the following format:
+							System.out.println("Computer:");
+							System.out.println("Brand:" + c.getBrand());
+							System.out.println("Computer model: " + c.getModel());
+							System.out.println("Computer SN: serial number:" + c.getSerialNumber());
+							System.out.println("Computer Price: $" + c.getPrice());
+						}
+					}
+				}
+
+				// while the update computers are greater than max computers and less than
+				// zero..continue
+				while (updateComputersNumber > maxComputers && updateComputersNumber < 0);
+
+				// what information would you like to change?
+				int choicemenu2 = kb.nextInt();
+
+				// Then ask the user which attribute they wish to change by displaying menu.
+				switch (choicemenu2) {
+				case 1:
+					System.out.println("Enter the new Brand:");
+					String brand = kb.next();
+
+					break;
+				case 2:
+					System.out.println("Enter the new Model:");
+					String model = kb.next();
+
+					break;
+				case 3:
+					System.out.println("Enter the new SN");
+					long serialNumber = kb.nextLong();
+
+					break;
+				case 4:
+					System.out.println("Enter the new Price:");
+					double price = kb.nextDouble();
+
+					break;
+				case 5:
+					System.out.println("Quit:");
+					break;
+				}
+
+				break;
+
+			case 3:
+				System.out.println("You choose 3. Display all computers by a specific brand");
+
+				// When option 3 is entered, prompt the user to enter a brand name.
+				// You then need to display the information of all computers that have that
+				// brand. (Hint: You may use a static method,
+				// for instance called findComputersBy , which accepts a string for a brand name
+				// then performs the needed search).
+
+				break;
+			case 4:
+				System.out.println("You choose 4. Display all computers by a specific price");
+
+				break;
+			case 5:
+				System.out.println("You choose 5 -> exit. The program finish");
+				break;
+			}
+		}
+		// while menu was different 5 continue ask
+		while (choiceMenu != 5);
+	}
+
+	// check if the password is correct
+	// I really needed to review and remember that scanner has a object type and
+	// that by putting it here my kb.next() no longer showed an error
+	public static boolean checkPassword(Scanner kb) {
+		// user has only 3 tries
+		int passwordTries = 0;
+
+		// I needed to remember the syntax equals, before that I tried only use "=="
+		// while the name password for different that "password" continue to prompt
+		while (passwordTries < 3) {
+			String namePassword = kb.next();
+			passwordTries++;
+
+			if (namePassword.equals("password")) {
+				return true;
+
+			} else {
+				// After the 3rd illegal entry, the main menu in figure 1 is re-displayed again.
+				System.out.println("Invalid, try again");
+			}
+		}
+		return false;
 	}
 }
--- src/Project/Computer1.java
+++ src/Project/Computer1.java
@@ -14,112 +14,168 @@ public class Computer1 {
 		System.out.println("-------------------------------------------------------------------------");
 		Scanner kb = new Scanner(System.in);
 
-		// owner put the number maximum of computers (Size or maxComputers)	
-		System.out.println("Whats the maximum numbers of computers that your store can contain? ");
-		int maxComputers = kb.nextInt();
+		int maxComputers = 0;
+		// initialize the variable main menu
+		int choiceMenu;
+		// initialize the variable for add new computers
+		int addNewComputers = 0;
+		// initialize update computers
+		int updateComputers = 0;
 
+		while (maxComputers == 0) {
+			// owner put the number maximum of computers (Size or maxComputers)
+			System.out.println("Whats the maximum numbers of computers that your store can contain? ");
+			maxComputers = kb.nextInt();
+
+			// test only with positives numbers
+			if (maxComputers < 1) {
+				System.out.println("Invalid, try again. The number should be positve");
+			}
+		}
 		// Create a empty array (inventory)
 		// Tracking the object computer - previously created
 		Computer inventory[] = new Computer[maxComputers];
 
-		// colocar dentro do while pra continuar mostrando o main menu
-		while (true) {		
-		// Display a main menu
-		System.out.println("You are on the Main menu:  ");
-		System.out.println("Select a option between 1 and 5");
-		System.out.println("---------------------------------------------------------------------------------");
-		System.out.println("Option 1: Enter new computers (Password Required)");
-		System.out.println("Option 2: Change information of a computer (Password required)");
-		System.out.println("Option 3: Display all computers by a specific brand");
-		System.out.println("Option 4: Display all computers under a certain a price");
-		System.out.println("Option 5: Quit");
-		System.out.println("---------------------------------------------------------------------------------");
-		System.out.println("Please enter your choice > ");
-		
-		// initialized a variably menu
-		// user choose a option between 1 and 5
-		int choiceMenu= kb.nextInt();
+		// Put do and while for continue show the main menu
+		do {
+			// Display a main menu
+			System.out.println("You are on the Main menu:  ");
+			System.out.println("---------------------------------------------------------------------------------");
+			System.out.println("Option 1: Enter new computers (Password Required)");
+			System.out.println("Option 2: Change information of a computer (Password required)");
+			System.out.println("Option 3: Display all computers by a specific brand");
+			System.out.println("Option 4: Display all computers under a certain a price");
+			System.out.println("Option 5: Quit");
+			System.out.println("---------------------------------------------------------------------------------");
+			System.out.println("Please enter your choice > ");
+			// initialized a variably menu
+			// user choose a option between 1 and 5
+			choiceMenu = kb.nextInt();
 
-		
-			// Each option
+			// Each main menu
 			switch (choiceMenu) {
 			case 1:
+
 				System.out.println("You choose 1.Put your password: ");
-				//if the correct password is check put how many computers do want to add
+				// if the correct password is check put how many computers do want to add
 				if (checkPassword(kb)) {
 					System.out.println("How many computers do want to add?");
+					addNewComputers = kb.nextInt();
+
+					// check if has enough space in the computer store
+					// if the new computers are more than capacity computers
+					if (addNewComputers > maxComputers) {
+						System.out.println("Please choose other number. This number exceeds tha maximum capacity");
+					}
 				}
+
+				// add new computers to the inventory
+				// put the quantity of the computers
+				for (int i = 0; i < addNewComputers; i++) {
+					
+					// i+1 because need show "computer 1" instead "computer 0"
+					System.out.println("Computer #: " + (i +1 ) + "was add to the inventory");
+					
+					/*System.out.println("Computer brand is: "  );
+					String brand = kb.next();	
+					
+					System.out.println("Computer price is: ");
+					double price = kb.nextDouble();		
+					
+					System.out.println("Computer model is: ");
+					String model = kb.next();
+					
+					System.out.println("Computer serial number is: ");
+					long serialNumber = kb.nextLong();
+					
+					*/System.out.println("-----------------------------");
+					computerCounter++;
+					
+
+				}
+
 				break;
-				
+
 			case 2:
-				System.out.println("You choose 2. Put your password");
-				//if the correct password is check put how many computers do want to add
-				if (checkPassword(kb)) {
-					System.out.println("Which computer do you want update?");
+
+				do {
+					System.out.println("You choose 2. Put your password");
+					// if the correct password is check put how many computers do want to add
+					if (checkPassword(kb)) {
+						System.out.println("Which computer number do you want update?");
+
+						// variable for update the computers
+						updateComputers = kb.nextInt();
+
+						// Loop for trough inside the number of computers
+						// The computer number is the index in the array inventory.
+						for (int i = 0; i < inventory.length; i++) {
+							if (updateComputers == 1) {
+
+								// display the current information of that computer in the following format:
+								System.out.println("Computer:");
+								System.out.println("Brand:");
+								System.out.println("Computer model: ");
+								System.out.println("Computer SN: serial number:");
+								System.out.println("Computer Price: $");
+							}
+						}
+					}
 				}
+				// while the update computers are greater than max computers and less than
+				// zero..continue
+				while (updateComputers > maxComputers && updateComputers < 0);
+
 				break;
+
 			case 3:
 				System.out.println("You choose 3. Display all computers by a specific brand");
+
 				break;
 			case 4:
-				System.out.println("You choose 4. Display all computers by a spacific price");
+				System.out.println("You choose 4. Display all computers by a specific price");
+
 				break;
 			case 5:
 				System.out.println("You choose 5 -> exit. The program finish");
+
 				break;
 
 			}
-			// while menu was different 5 continue ask
-		while (choiceMenu != 5);
 		}
-		
+
+		// while menu was different 5 continue ask
+		while (choiceMenu != 5);
 	}
-	
+
 	// check if the password is correct
 	// I really needed to review and remember that scanner has a object type and
 	// that by putting it here my kb.next() no longer showed an error
 	public static boolean checkPassword(Scanner kb) {
 		// user has only 3 tries
-		int passwordTries = 3;
-		
+		int passwordTries = 0;
+
 		// I needed to remember the syntax equals, before that I tried only use "=="
 		// while the name password for different that "password" continue to prompt
-		while (passwordTries > 0) {
+		while (passwordTries < 3) {
 			String namePassword = kb.next();
-					
-		if (namePassword.equals("password")) {
-			return true;
+			passwordTries++;
 
-		}else  {
-			//TENTAR COLOCAR ISSO APENAS TRES TEANTATIVAS E PRINT O MENU, NAO TO CONSEGUINDO COLOCAR AINDA
-			//COLCOAR PRA NAO ACEITAR NUMERO NEGATIVO
-			//After the 3rd illegal entry, the main menu in figure 1 is re-displayed again.
-			System.out.println("Invalid, try again");
-			
+			if (namePassword.equals("password")) {
+				return true;
+
+			} else {
+
+				// After the 3rd illegal entry, the main menu in figure 1 is re-displayed again.
+				System.out.println("Invalid, try again");
+
+			}
 		}
-		}		
 		return false;
 
 	}
-	//create a new method for update information
-	//put the quantity of the computers
+	
+	
 
-	// print the information of an object
-	// make get for brand, price, model and Serial number
-		public static void displayComputer(Computer c, Scanner kb) {
-
-		System.out.println("The info is : ");
-		System.out.println("Computer brand is : " + c.getBrand());
-		System.out.println("Computer price is : " + c.getPrice());
-		System.out.println("Computer model is : " + c.getModel());
-		System.out.println("Computer serial number is : " + c.getSerialNumber());
-		System.out.println("-----------------------------");
-		
-		//choose the second menu
-		int choiceMenu2 = kb.nextInt();
-		
-	}
-			
-			
-		
+	
 }
